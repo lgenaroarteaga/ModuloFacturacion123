f891b64efcabc8bf0c23f3ee66f3597d276b2ba9
Luis Genaro Arteaga Salinas <larteaga@info-arch.com>
2020-02-28 22:10:52 -0400

    Valueobject for numeric string validation with regex

---
+++ ModuloFacturacion/ModuloFacturacion/Models/ValueObject/Types/NumericString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace ModuloFacturacion.Models.ValueObject.Types
+{
+    public class NumericString : ValueObject<NumericString>
+    {
+        protected NumericString() { }
+
+        public string Value { get; internal set; }
+
+        public static NumericString FromInt(int value) => new NumericString(value.ToString());
+        public static NumericString FromString(string value) => new NumericString(value);
+
+        internal NumericString(string value)
+        {
+            CheckValidity(value);
+            Value = value.Trim();
+        }
+
+        protected override bool CheckValidity(object value)
+        {
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+                throw new ArgumentException("Can't be an empty or null string the name");
+            if (!Regex.IsMatch(value.ToString().Trim(), @"^\d+$"))
+            {
+                throw new ArgumentException("String is not a numeric value");
+            }
+            String strValue = (String)value;
+            if (strValue.Length > 255)
+                if (strValue.Trim().Length > 255)
+                throw new ArgumentException("Can't be more than 255 characters long");
+            return true;
+        }
+
+        public static implicit operator NumericString(string value)
+        {
+            return new NumericString(value);
+        }
+    }
+}
